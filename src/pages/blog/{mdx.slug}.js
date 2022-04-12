1c8073bd8a45bc833ba638931992793f31853303
casanova <alejandro.casanova.martin@alumnos.upm.es>
2022-04-12 16:47:02 +0200

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,16 +2,22 @@ import * as React from 'react'
 import Layout from '../../components/layout'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 //Syntax highlighting
 require("prismjs/themes/prism-solarizedlight.css");
 require("prismjs/plugins/command-line/prism-command-line.css");
 
 const BlogPost = ({data}) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
 
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
@@ -20,12 +26,20 @@ const BlogPost = ({data}) => {
 }
 
 export const query = graphql`
-  query($id: String) {
+  query($id: String){
     mdx(id: {eq: $id}) {
       id
       frontmatter {
         date(formatString: "YYYY, MMMM Do")
         title
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
